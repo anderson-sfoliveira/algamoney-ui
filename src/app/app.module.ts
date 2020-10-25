eaa5f102fe2fa290f513e41055b12f0a2048614e
Anderson Oliveira <anderson@sfoliveira@gmail.com>
2020-10-25 14:26:03 -0300

    Cadastro Financeiro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,25 +5,29 @@ import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { TableModule } from 'primeng/table';
 import { TooltipModule } from 'primeng/tooltip';
+import { InputTextareaModule } from 'primeng/inputtextarea';
 
 import { AppComponent } from './app.component';
 import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LancamentosCadastroComponent } from './lancamentos-cadastro/lancamentos-cadastro.component';
+import { FinanceiroCadastroComponent } from './financeiro-cadastro/financeiro-cadastro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LancamentosPesquisaComponent,
     NavbarComponent,
-    LancamentosCadastroComponent
+    LancamentosCadastroComponent,
+    FinanceiroCadastroComponent
   ],
   imports: [
     BrowserModule,
     InputTextModule,
     ButtonModule,
     TableModule,
-    TooltipModule
+    TooltipModule,
+    InputTextareaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/financeiro-cadastro/financeiro-cadastro.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-financeiro-cadastro',
+  templateUrl: './financeiro-cadastro.component.html',
+  styleUrls: ['./financeiro-cadastro.component.css']
+})
+export class FinanceiroCadastroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
