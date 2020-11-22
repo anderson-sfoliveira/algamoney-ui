9bbce4fd97efffb944ec802ce0055f71c29eafdd
Anderson Oliveira <anderson@sfoliveira@gmail.com>
2020-11-22 01:41:42 -0300

    botao

--- src/app/botoes/botoes.module.ts
+++ src/app/botoes/botoes.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BotaoAlertaComponent } from './botao-alerta/botao-alerta.component';
 
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
+
 
 
 @NgModule({
   declarations: [BotaoAlertaComponent],
   imports: [
-    CommonModule
+    CommonModule,
+
+    ButtonModule,
+    RippleModule
   ],
   exports: [BotaoAlertaComponent]
 })
