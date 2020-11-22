07fde4a4fa5a7221ad690b90367e9755527ea2d7
Anderson Oliveira <anderson@sfoliveira@gmail.com>
2020-11-22 01:11:23 -0300

    Novo modulo

---
+++ src/app/botoes/botao-teste/botao-teste.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-botao-teste',
+  templateUrl: './botao-teste.component.html',
+  styleUrls: ['./botao-teste.component.css']
+})
+export class BotaoTesteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
