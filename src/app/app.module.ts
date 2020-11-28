faae872791dba2cceb8064747410a06b2c8b31a8
Anderson Oliveira <anderson@sfoliveira@gmail.com>
2020-11-28 02:18:46 -0300

    ajuste componente navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 
-import { InputTextModule } from 'primeng/inputtext';
-import { ButtonModule } from 'primeng/button';
-import { TableModule } from 'primeng/table';
-import { TooltipModule } from 'primeng/tooltip';
-import { InputTextareaModule } from 'primeng/inputtextarea';
-import { CalendarModule } from 'primeng/calendar';
-import { SelectButtonModule } from 'primeng/selectbutton';
-import { DropdownModule } from 'primeng/dropdown';
-import { InputNumberModule } from 'primeng/inputnumber';
-import { CheckboxModule } from 'primeng/checkbox';
-import { InputMaskModule } from 'primeng/inputmask';
-
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
 import { MessageComponent } from './message/message.component';
 
 import { NavegacaoModule } from './navegacao/navegacao.module';
@@ -28,25 +15,12 @@ import { FormaPagamentoModule } from './forma-pagamento/forma-pagamento.module';
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
     MessageComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
-
-    InputTextModule,
-    ButtonModule,
-    TableModule,
-    TooltipModule,
-    InputTextareaModule,
-    CalendarModule,
-    SelectButtonModule,
-    DropdownModule,
-    InputNumberModule,
-    CheckboxModule,
-    InputMaskModule,
 
     NavegacaoModule,
     LancamentosModule,
--- src/app/navegacao/navegacao.module.ts
+++ src/app/navegacao/navegacao.module.ts
@@ -1,17 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { MenuComponent } from './menu/menu.component';
-import { BotoesModule } from '../botoes/botoes.module';
 import { LinhaComponent } from './linha/linha.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
-
+import { BotoesModule } from '../botoes/botoes.module';
 
 @NgModule({
-  declarations: [MenuComponent, LinhaComponent],
+  declarations: [
+    MenuComponent,
+    LinhaComponent,
+    NavbarComponent
+  ],
   imports: [
     CommonModule,
     BotoesModule
   ],
-  exports: [MenuComponent]
+  exports: [
+    MenuComponent,
+    NavbarComponent
+  ]
 })
 export class NavegacaoModule { }
