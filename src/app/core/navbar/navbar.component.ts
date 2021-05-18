eece75d00e316eb67d48195745ac0f00236ba99b
Anderson Oliveira <2175235+anderson-sfoliveira@users.noreply.github.com>
2021-05-18 18:18:04 -0300

    Exibindo o nome do usuário logado no navbar

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthService } from 'src/app/seguranca/auth.service';
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+
+  constructor (
+    public auth: AuthService
+  ) {}
 
   exibindoMenu = false;
 
