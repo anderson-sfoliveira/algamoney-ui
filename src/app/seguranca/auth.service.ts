4c3d928c2c783bdc3553b0339f40884586fc991b
Anderson Oliveira <2175235+anderson-sfoliveira@users.noreply.github.com>
2021-05-18 13:18:14 -0300

    Decodificando o JWT e armazenando no Local Storage

--- src/app/seguranca/auth.service.ts
+++ src/app/seguranca/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
@@ -7,8 +8,14 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   oauthTokenURL = 'http://localhost:8080/oauth/token';
+  jwtPayload: any; // objeto com várias as propriedades do token
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private jwtHelperService: JwtHelperService
+  ) {
+    this.carregarToken();
+  }
 
   login(usuario: string, senha: string): Promise<void> {
     const headers = new HttpHeaders()
@@ -17,13 +24,28 @@ export class AuthService {
 
     const body = `username=${usuario}&password=${senha}&grant_type=password`;
 
-    return this.http.post(this.oauthTokenURL, body, { headers })
+    return this.http.post<any>(this.oauthTokenURL, body, { headers })
       .toPromise()
       .then(response => {
         console.log(response);
+        this.armazenarToken(response.access_token);
       })
       .catch(response => {
         console.log(response);
       });
+  }
+
+  private armazenarToken(token: string) {
+    this.jwtPayload = this.jwtHelperService.decodeToken(token);
+    console.log(this.jwtPayload);
+    localStorage.setItem('token', token);
+  }
+
+  private carregarToken() {
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      this.armazenarToken(token);
+    }
   }
 }
--- src/app/seguranca/seguranca.module.ts
+++ src/app/seguranca/seguranca.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
-import { LoginFormComponent } from './login-form/login-form.component';
-import { SegurancaRoutingModule } from './seguranca-routing.module';
+import { JwtHelperService, JwtModule } from '@auth0/angular-jwt';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { PasswordModule } from 'primeng/password';
+
+import { LoginFormComponent } from './login-form/login-form.component';
+import { SegurancaRoutingModule } from './seguranca-routing.module';
 
 @NgModule({
   declarations: [
@@ -15,12 +17,20 @@ import { PasswordModule } from 'primeng/password';
   imports: [
     CommonModule,
     FormsModule,
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: () => {
+          return '';
+        }
+      }
+    }),
 
     InputTextModule,
     ButtonModule,
     PasswordModule,
 
     SegurancaRoutingModule
-  ]
+  ],
+  providers: [JwtHelperService]
 })
 export class SegurancaModule { }
