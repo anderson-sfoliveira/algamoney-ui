1b532a974f6a83a42b3ebb3bf5cdd8fb8e28ca3f
Anderson Oliveira <2175235+anderson-sfoliveira@users.noreply.github.com>
2021-05-19 15:18:16 -0300

    Update seguranca.module.ts

--- src/app/seguranca/seguranca.module.ts
+++ src/app/seguranca/seguranca.module.ts
@@ -12,6 +12,7 @@ import { LoginFormComponent } from './login-form/login-form.component';
 import { SegurancaRoutingModule } from './seguranca-routing.module';
 import { SegurancaHttpInterceptor } from './seguranca-http-interceptor';
 import { AuthGuard } from './auth.guard';
+import { environment } from 'src/environments/environment';
 
 export function tokenGetter(): string {
   return localStorage.getItem('token');
@@ -28,7 +29,9 @@ export function tokenGetter(): string {
       config: {
         tokenGetter, // Adiciona o Access Token nas chamadas HTTP
         allowedDomains: ['localhost:8080'],
-        disallowedRoutes: ['http://localhost:8080/oauth/token']
+//        disallowedRoutes: ['http://localhost:8080/oauth/token']
+//        allowedDomains: [`${environment.apiURL}`],
+        disallowedRoutes: [`${environment.apiURL}/oauth/token`]
       }
     }),
 
