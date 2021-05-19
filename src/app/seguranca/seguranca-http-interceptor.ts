2de91e92861e1184c55ca379edd96347fbda3fbd
Anderson Oliveira <2175235+anderson-sfoliveira@users.noreply.github.com>
2021-05-19 11:44:55 -0300

    Update seguranca-http-interceptor.ts

--- src/app/seguranca/seguranca-http-interceptor.ts
+++ src/app/seguranca/seguranca-http-interceptor.ts
@@ -12,6 +12,18 @@ export class SegurancaHttpInterceptor implements HttpInterceptor {
   constructor(private auth: AuthService) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+    /*
+     * Primeiramente fazemos duas validações, uma pra saber se não estamos nos referindo ao path "/oauth/token" e
+     * outra para sabermos se o nosso token está inválido.
+     * 
+     * 1) Se o path for /oauth/token
+     *    Neste caso, estamos fazendo uma busca por um token válido, o que significa que nosso token atual já foi invalidado.
+     *    Se não validarmos o request para este path, entraremos em um loop infinito.
+     * 
+     * 2) Se o token está inválido
+     *    Aqui checamos se nosso token está inválido, e se estiver, precisamos obter um novo, através do "/oauth/token"
+     */
     if (!req.url.includes('/oauth/token') && this.auth.isAccessTokenInvalido()) {
       return from(this.auth.obterNovoAccessToken())
         .pipe(
