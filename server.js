1f61ac77c71a6a523b2c6cacd99af3b8ca3a6d5d
Anderson Oliveira <2175235+anderson-sfoliveira@users.noreply.github.com>
2021-05-24 21:49:56 -0300

    Configurando a aplicação para subir para o Heroku

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ app.get('/*', function(req, res) {
   res.sendFile(__dirname + '/dist/artisapp-ui/index.html');
 });
 
-app.listen(4200);
+//app.listen(4200);
+app.listen(process.env.PORT || 4200) ;
+// No Heroku sempre terá uma variavel de ambiente "PORT".
 
 // comando para subir o servidor => node server.js
