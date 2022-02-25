001b45669b6fbf4e3106f5dfc2ad94155cde72ae
Anderson Oliveira <2175235+anderson-sfoliveira@users.noreply.github.com>
2022-02-25 09:43:20 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   apiURL: 'https://algamoney-api-mycloud.herokuapp.com',
 
-  tokenWhitelistedDomains: [ new RegExp('brl-empresa-api.herokuapp.com') ],
+  tokenWhitelistedDomains: [ new RegExp('algamoney-api-mycloud.herokuapp.com') ],
   tokenBlacklistedRoutes: [ new RegExp('\/oauth\/token') ]
 };
